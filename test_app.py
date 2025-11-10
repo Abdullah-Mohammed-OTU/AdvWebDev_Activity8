239ebaf55466f626579ef7130a568e3e0e56d4bf
abdullah <abdullahmohammed583@outlook.com>
2025-11-10 14:39:18 -0500

    modified code so it would pass linting tests

--- test_app.py
+++ test_app.py
@@ -2,11 +2,13 @@
 import app
 from flask import json
 
+
 def test_hello():
     client = app.app.test_client()
     response = client.get('/hello')
     assert response.status_code == 200
     assert response.get_json() == {"message": "Hello, World!"}
+
 
 def test_echo():
     client = app.app.test_client()
