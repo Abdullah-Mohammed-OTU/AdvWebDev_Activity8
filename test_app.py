1bf5b4f19973509817925bb72832877a115b5624
abdullah <abdullahmohammed583@outlook.com>
2025-11-10 14:20:12 -0500

    Add echo endpoint and corresponding test case

--- test_app.py
+++ test_app.py
@@ -1,8 +1,16 @@
 # test_app.py
 import app
+from flask import json
 
 def test_hello():
     client = app.app.test_client()
     response = client.get('/hello')
     assert response.status_code == 200
     assert response.get_json() == {"message": "Hello, World!"}
+
+def test_echo():
+    client = app.app.test_client()
+    payload = {"msg": "ping"}
+    response = client.post('/echo', json=payload)
+    assert response.status_code == 201
+    assert response.get_json() == payload
